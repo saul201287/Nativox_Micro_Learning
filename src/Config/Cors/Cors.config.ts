5ddd03d42e14878f2623a6d4b0686e8aec01af5c
saul201287 <84940926+saul201287@users.noreply.github.com>
2025-07-22 19:57:22 -0600

    cors

--- src/Config/Cors/Cors.config.ts
+++ src/Config/Cors/Cors.config.ts
@@ -2,5 +2,6 @@ export const corsOptions = {
   origin: ["*"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
   optionsSuccessStatus: 200,
 };
